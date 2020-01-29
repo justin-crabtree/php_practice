8861b8e8ab18651bd179acf56eeb758745ce749b
crabtree <justin.kyle.crabtree@gmail.com>
2020-01-29 16:33:01 -0500

    adds arrays.php file

---
+++ arrays.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php 
+
+$numList = [1, 2, 4, 8];
+
+print_r($numList)
+echo $numList[0];
+
+
+
+
+?>
+
+
+
+
+</body>
+</html>
