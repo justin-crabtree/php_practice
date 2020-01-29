50d487775a6b151c3f1d6d95654838058e683ec0
crabtree <justin.kyle.crabtree@gmail.com>
2020-01-29 16:09:32 -0500

    adds variables.php file

---
+++ variables.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+<?php 
+
+$name = "Justin";
+$number = 100;
+
+// echo "$name";
+echo $name . " " . $number;
+
+
+?>
+
+
+
+
+</body>
+</html>
