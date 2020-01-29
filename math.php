637b492053f8b6412a3e020d7048cd2b83d46a8e
crabtree <justin.kyle.crabtree@gmail.com>
2020-01-29 16:21:07 -0500

    adds math.php file

---
+++ math.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php 
+
+echo 50 + 50;
+echo "<br>";
+echo 50 / 2;
+echo "<br>";
+
+$num1 = 12;
+$num2 = 24;
+
+echo $num2 * $num1
+
+?>
+
+
+
+</body>
+</html>
