6fe5d2058df85bb159f59faf52f56d9181959634
crabtree <justin.kyle.crabtree@gmail.com>
2020-01-29 20:15:26 -0500

    adds arrays_assoc.php file

---
+++ arrays_assoc.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+
+<?php 
+
+$num = [10, 20, 30];
+print_r($num);
+
+// echo $num[0];
+echo "<br>";
+
+$names = ["first_name" => 'Justin', "last_name" => 'Crabtree'];
+print_r($names);
+
+echo "<br>";
+
+echo $names["first_name"] . " " . $names["last_name"];
+
+
+
+?>
+
+
+
+
+
+</body>
+</html>
