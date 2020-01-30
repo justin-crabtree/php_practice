95de0dc69604fd6cd5d0328de324857ee1a6d55c
crabtree <justin.kyle.crabtree@gmail.com>
2020-01-30 17:34:32 -0500

    adds if_statements.php file

---
+++ if_statement.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php 
+
+if(3 > 10){
+
+  echo "three is less than ten";
+
+
+} elseif (4 < 5) {
+  echo "elseif is true";
+}
+
+
+else {
+
+  echo "its not";
+
+}
+
+
+
+
+
+?>
+
+
+
+
+
+</body>
+</html>
