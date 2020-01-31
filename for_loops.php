0595e9e309bae304fafc7d54f4077fbbe06d53ea
crabtree <justin.kyle.crabtree@gmail.com>
2020-01-30 20:45:05 -0500

    adds for_loops.php file

---
+++ for_loops.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php 
+
+
+for($i = 0; $i < 10; $i++){
+
+echo "Ora Ora Ora<br>";
+
+}
+
+
+
+?>
+
+
+
+
+
+
+</body>
+</html>
