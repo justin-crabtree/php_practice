b26505304795f0b84afd394d4049d34cfd97ff4b
crabtree <justin.kyle.crabtree@gmail.com>
2020-01-30 20:17:16 -0500

    adds switch_statements.php file

---
+++ switch_statements.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php 
+
+$num = 100;
+
+switch ($num) {
+  case 34:
+    echo "it is 34";
+    break;
+  case 2:
+    echo "it is 2";
+    break;
+  case 5:
+    echo "it is 5";
+    break;
+  case 10:
+    echo "it is 10";
+    break;
+
+  default:
+    echo "we cant find anything";
+    break;
+}
+
+
+
+
+?>
+
+
+
+
+</body>
+</html>
