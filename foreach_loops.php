43e5dbe1a7e2c60f439de2217d334982f2182e9f
crabtree <justin.kyle.crabtree@gmail.com>
2020-01-30 20:53:10 -0500

    adds foreach_loops.php file

---
+++ foreach_loops.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php 
+
+$nums = [1, 2, 3, 4, 5];
+
+// assigns each of the nums the key of "num"
+foreach ($nums as $num) {
+  echo $num . "<br>";
+}
+
+
+
+
+?>
+
+
+
+
+
+
+</body>
+</html>
