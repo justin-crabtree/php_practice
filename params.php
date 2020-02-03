af45fc065381fb2ee8e9488f1150b30cd99c4c4b
crabtree <justin.kyle.crabtree@gmail.com>
2020-02-03 18:27:58 -0500

    adds params.php file

---
+++ params.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php 
+
+// function game_Quote($message){
+//   echo $message;
+
+
+// }
+
+// game_Quote("There was a hole here. It's gone now.");
+
+function calculate($number_1, $number_2){
+  $sum = $number_1 + $number_2;
+  echo $sum;
+
+}
+
+calculate(5, 5);
+
+
+
+
+?>
+
+
+
+
+
+
+</body>
+</html>
