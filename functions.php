c8b177d992f16b8135a9165944aede24c0582206
crabtree <justin.kyle.crabtree@gmail.com>
2020-02-03 17:23:21 -0500

    adds functions.php

---
+++ functions.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php 
+
+function silent_Hill_2(){
+  display_Game();
+  display_Phrase();
+}
+
+
+function display_Phrase(){
+  echo "'There was a hole here. It's gone now.'";
+
+}
+
+function display_Game(){
+  echo 'Silent Hill 2: ';
+
+}
+
+silent_Hill_2();
+
+?>
+
+
+
+
+
+
+</body>
+</html>
