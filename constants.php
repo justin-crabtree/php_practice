6995061df7815c265d522256a222955c87aa4940
crabtree <justin.kyle.crabtree@gmail.com>
2020-02-04 17:06:14 -0500

    adds constants.php file

---
+++ constants.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php 
+
+$number = 5;
+
+echo $number . "<br>";
+
+const CONSTANT = 'Ora Ora Ora';
+
+echo CONSTANT;
+
+
+?>
+
+
+
+
+
+
+</body>
+</html>
