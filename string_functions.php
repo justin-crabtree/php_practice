e3fad3cdbf1dedce05be4c7c8258122fad04649d
crabtree <justin.kyle.crabtree@gmail.com>
2020-02-05 18:39:16 -0500

    adds string_functions.php file

---
+++ string_functions.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php 
+
+
+$string = "There was a hole here. It's gone now";
+
+echo strlen($string); // displays the amount of characters in a string
+echo "<br>";
+
+echo strtoupper($string); // capitalize the entire string
+echo "<br>";
+
+echo strtolower($string); // lowercase the entire string
+echo "<br>";
+
+?>
+
+
+
+
+
+
+</body>
+</html>
