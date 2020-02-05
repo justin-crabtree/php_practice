69cc0b0ac947265e3af62c1d080bbf05858f9a44
crabtree <justin.kyle.crabtree@gmail.com>
2020-02-05 17:48:08 -0500

    adds math_functions.php file

---
+++ math_functions.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php 
+
+echo pow(2, 7); // pow("base", "exponent")
+
+echo "<br>";
+
+echo rand(1, 100); // random number between 1-100; leave blank for any random number
+
+echo "<br>";
+
+echo sqrt(100); // square root of 100
+
+echo "<br>";
+
+echo ceil(4.6); // rounds number up based on integer
+echo "<br>";
+
+echo floor(4.6); // round number down based on integer
+echo "<br>";
+
+echo round(4.4); // round number based on decimal
+?>
+
+
+
+
+
+
+</body>
+</html>
