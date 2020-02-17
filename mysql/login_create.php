7c298712d3de6d12a24e2965d55b924056a5fa0a
crabtree <justin.kyle.crabtree@gmail.com>
2020-02-17 16:33:51 -0500

    adds query to create username/password for database

---
+++ mysql/login_create.php
@@ -0,0 +1,90 @@
+<?php 
+
+if(isset($_POST['submit'])) {
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $connection = mysqli_connect('localhost', 'root', '', 'login_app');
+    if ($connection) {
+      echo "it works";
+    } else {
+      die("Database connection failed");
+    }
+
+    $query = "INSERT INTO users(username, password) ";
+    $query .= "VALUES ('$username', '$password')";
+
+    $result = mysqli_query($connection, $query);
+
+    if (!$result) {
+      die("query failed" . mysqli_error());
+    }
+
+
+
+
+
+
+  // if($username && $password) {
+  //   echo $username;
+  //   echo $password;
+  // } else {
+  //   echo "denied";
+  // }
+
+  
+
+
+}
+
+
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+  <div class="col-xs-6">
+    <form action="login_create.php" method="post">
+
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" name="username" class="form-control">
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" class="form-control">
+      </div>
+
+      <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+
+    </form>
+  </div>
+
+
+
+</div>
+  
+
+
+
+
+
+
+
+</body>
+</html>
