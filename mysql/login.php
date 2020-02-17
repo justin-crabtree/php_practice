fa6d69762352ac8c2edc42c28906841a16d70e90
crabtree <justin.kyle.crabtree@gmail.com>
2020-02-17 16:03:22 -0500

    adds mysql folder and login.php file

---
+++ mysql/login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+  <div class="col-xs-6">
+    <form action="login.php" method="post">
+      
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" name="username" class="form-control">
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" class="form-control">
+      </div>
+
+      <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+
+    </form>
+  </div>
+
+
+
+</div>
+  
+<?php 
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+</body>
+</html>
