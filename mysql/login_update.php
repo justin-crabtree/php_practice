f3e4b0def5646d248236cefdf26db3a184ff5611
crabtree <justin.kyle.crabtree@gmail.com>
2020-02-19 15:17:43 -0500

    adds login_update.php and db.php file

---
+++ mysql/login_update.php
@@ -0,0 +1,62 @@
+<?php include 'db.php';
+
+
+$query = "SELECT * FROM users";
+$result = mysqli_query($connection, $query);
+if (!$result) {
+  die("query failed" . mysqli_error());
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+  <div class="col-xs-6">
+    
+    <form action="login_create.php" method="post">
+
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" name="username" class="form-control">
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" class="form-control">
+      </div>
+
+      <div class="form-group">
+        <select name="" id="">
+          <option value="">1</option>
+        </select>
+      </div>
+
+      <input class="btn btn-primary" type="submit" name="submit" value="Update">
+
+    </form>
+
+  </div>
+
+
+
+</div>
+  
+
+
+
+
+
+
+
+</body>
+</html>
