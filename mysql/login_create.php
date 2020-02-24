de0bcd12f500a70b5d5bdd19953351ef533dbd9d
crabtree <justin.kyle.crabtree@gmail.com>
2020-02-24 18:09:59 -0500

    adds includes folder with header.php file, refactors CRUD files with headers

--- mysql/login_create.php
+++ mysql/login_create.php
@@ -1,19 +1,14 @@
 <?php 
 include 'db.php';
 include 'functions.php';
+include 'includes/header.php';
 
 createRows();
 
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Document</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
+
 <body>
 
 <div class="container">
@@ -40,12 +35,5 @@ createRows();
 
 </div>
   
-
-
-
-
-
-
-
 </body>
 </html>
