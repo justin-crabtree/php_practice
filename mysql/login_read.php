8f470a2f1643d8d100d940484879d5f3904e42b0
crabtree <justin.kyle.crabtree@gmail.com>
2020-02-24 18:21:35 -0500

    adds readRows function to functions.php, refactors login_read.php

--- mysql/login_read.php
+++ mysql/login_read.php
@@ -1,51 +1,20 @@
 <?php 
-
-
-$connection = mysqli_connect('localhost', 'root', '', 'login_app');
-  if ($connection) {
-    echo "You're Connected";
-  } else {
-    die("Database connection failed");
-  }
-
-  $query = "SELECT * FROM users";
-
-  $result = mysqli_query($connection, $query);
-
-  if (!$result) {
-    die("query failed" . mysqli_error());
-  }
-
-
+include "db.php";
+include 'includes/header.php';
+include 'functions.php';
 
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Document</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
+
 <body>
 
 <div class="container">
   <div class="col-xs-6">
-
-    <?php
-    while($row = mysqli_fetch_assoc($result)) {
-    ?>
-
     <pre>
-      <?php
-      print_r($row);
-      ?>
+      <?php readRows(); ?>
     </pre>
-    <?php
-      }
 
-      ?>
   </div>
 
 
