56f351369c745cadd394ee39638cf0fb6c37d47b
crabtree <justin.kyle.crabtree@gmail.com>
2020-02-25 20:16:44 -0500

    adds real escape string function to username and password

--- mysql/functions.php
+++ mysql/functions.php
@@ -8,6 +8,9 @@ function createRows() {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
+
     $query = "INSERT INTO users(username, password) ";
     $query .= "VALUES ('$username', '$password')";
 
