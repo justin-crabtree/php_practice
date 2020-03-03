5313af3533c036cb01db4799bad6d5b998253edf
crabtree <justin.kyle.crabtree@gmail.com>
2020-03-03 18:08:16 -0500

    adds session1 and 2.php files

---
+++ session1.php
@@ -0,0 +1,37 @@
+<?php 
+
+session_start();
+$_SESSION['message'] = "There was a hole here. It's gone now.";
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php 
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+</body>
+</html>
