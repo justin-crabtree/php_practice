1744d0213af2795b3d491ddeddcb7eb006ed4817
crabtree <justin.kyle.crabtree@gmail.com>
2020-03-05 16:22:14 -0500

    adds function to change number of wheels, creates new instance for tesla and semi

--- class_car_3.php
+++ class_car_3.php
@@ -1,18 +1,36 @@
 <?php 
 
 class Car {
-  function moveWheels() {
-    echo "Wheels move";
+  var $wheels = 4;
+  var $engine = 1;
+  var $doors = 4;
+
+  function amountWheels() {
+    // "$this" refers to the class its inside of, in this case "Car"
+    $this->wheels = 10;
   }
 }
 
-$bmw = new Car();
-$merz = new Car();
-$honda = new Car();
+// assigns var to new instance of Car
+$tesla = new Car();
+$semi = new Car();
 
-$bmw->moveWheels(); 
-$merz->moveWheels();
-$honda->moveWheels();
+// calls on function which changes the var of wheels to == 10 for the instance of $semi
+$semi->amountWheels();
+
+
+echo $tesla->wheels . "<br>";
+echo $semi->wheels;
+
+
+
+
+// $merz = new Car();
+// $honda = new Car();
+
+// $bmw->moveWheels(); 
+// $merz->moveWheels();
+// $honda->moveWheels();
 
 
 ?>
